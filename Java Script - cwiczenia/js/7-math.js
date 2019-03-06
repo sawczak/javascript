bcc64c308a64f42339dcbba6a19e9a7e2837a9fa
Izabela Sawczak <izabela.ziemba@gmail.com>
2019-03-06 21:31:27 +0100

    zadanie-4-wykonanie

--- Java Script - cwiczenia/js/7-math.js
+++ Java Script - cwiczenia/js/7-math.js
@@ -1,36 +1,4 @@
 // const liczbaRandomowa = Math.floor(Math.random() * 20);  //math floor stałe liczby, bez tego będzie pokazywac liczby po przecinku
- // pokaże liczby max do 20 / math.ceil - zaokrągla w górę
-
-
+//  pokaże liczby max do 20 / math.ceil - zaokrągla w górę
 
 //  console.log(liczbaRandomowa);
-
-
-/* 
-let napis = ('Akademia108') => {
-    const rozdzielenie = napis.split();
-    const odwracamy = rozdzielenie.reverse(); 
-    const laczymy = odwracamy.join();
-
-}
-
-console.log(laczymy); */
-
-
-  /*   const napisWspak = (tekst) => {
-        return tekst.split('').reverse('').join('');
-    }   
-    
-    console.log(napisWspak('Akademia108')); */
-
-
-    
-    
-    let wspak = function(napis) {
-        let rozdzielamy = napis.split('');
-        let odwracamy = rozdzielamy.reverse('');
-        let laczymy = odwracamy.join('');
-        
-        return laczymy;
-        }
-        console.log(wspak('Akademia108'));
--- zadania-domowe/4-suma-kwadratow/js/main.js
+++ zadania-domowe/4-suma-kwadratow/js/main.js
@@ -1,12 +1,51 @@
-let tablica = [0,1,2,3,4,5];
 
+let tablica = [0,1,2,3,4,5,6];
 
-let mnozenie = 1;
+function sumaKwadratow(tablicaSuma) {
+  let sumaK = 0;
 
-for (let index = 0; index < tablica.length; index++) {
-        suma += tablica[index];
-        mnozenie *= tablica[index];
+  for (let index = 0; index < tablicaSuma.length; index++) {
+      sumaK += Math.pow(tablicaSuma[index], 2);
     }
+  return sumaK;  
+}
 
-console.log(suma, mnozenie);
+let wynikDzialaniaFunkcji = sumaKwadratow(tablica);
+console.log( wynikDzialaniaFunkcji );
 
+
+// pętla for z dwoma tablicami
+let tablica = [0,1,2,3,4,5,6];
+let drugaTablica = [0,1,2,3,4,5,6,7,8,9,10];
+
+function sumaKwadratow(tablicaSuma) {
+  let sumaK = 0;
+
+  for (let index = 0; index < tablicaSuma.length; index++) {
+      sumaK += Math.pow(tablicaSuma[index], 2);
+    }
+  return sumaK;  
+}
+
+let wynikDzialaniaFunkcji = sumaKwadratow(tablica);
+console.log( wynikDzialaniaFunkcji );
+
+let wynikDrugiej = sumaKwadratow(drugaTablica);
+console.log( wynikDrugiej );
+
+
+
+//z wykorzystaniem pętli forEach
+let tablica = [0, 1, 2, 3, 4, 5];
+
+function sumaKwadratow(parametrFunkcji) {
+  let sumaK = 0;
+  parametrFunkcji.forEach((element, index) => {
+    sumaK += Math.pow(element, 2);
+  });
+
+  return sumaK;
+}
+
+let wynik = sumaKwadratow(tablica);
+console.log(wynik);
